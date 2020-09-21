10e9d238e0c91bdd58025bde70c7e97763ab806e
윤선영 <seonyeongyun@naver.com>
2020-09-21 17:26:02 +0900

    Add Django

--- 일봉_분석/VIX-NaSD예측_익일나스닥(일봉)2020.R
+++ 일봉_분석/VIX-NaSD예측_익일나스닥(일봉)2020.R
@@ -28,27 +28,31 @@ merge <- merge(V, N_n, by='일자 / 시간')
 View(merge)
 head(merge)
 
+# 2020년부터 해당되는 자료 정리
+str(merge)
+merge_new <- merge[which(substr(merge$`일자 / 시간`,1,4)==2020),]
+View(merge_new)
 
 # 상관 관계 분석
-cor(merge$종가, merge$변동폭)
-cor.test(merge$종가, merge$변동폭) 
+cor(merge_new$종가, merge_new$변동폭)
+cor.test(merge_new$종가, merge_new$변동폭) 
 #p-value < 0.5(유의수준) 이므로 두 변수는 상관관계를 가짐.
 
 
 
 # 회귀 분석
-m1 <- lm(변동폭~종가, merge)
+m1 <- lm(변동폭~종가, merge_new)
 
-# y=10.99x-52.71
+# y=9.844x-9.942
 
 summary(m1)
 # p-값은 0.5 이하로 회귀계수의 추정치들이 통계적으로 유의
-# 결정계수(R-squared)=0.5373로, 회귀식이 데이터를 적절하게 설명하고 있다고 볼 수 없다.
+# 결정계수(R-squared)=0.3778로, 회귀식이 데이터를 적절하게 설명하고 있다고 볼 수 없다.
 
 
-plot(변동폭 ~ 종가, data= merge)
-abline( h = mean(merge$변동폭), lty=2, col = 'blue')
-abline( v = mean(merge$종가), lty=2, col = 'blue')
+plot(변동폭 ~ 종가, data= merge_new)
+abline( h = mean(merge_new$변동폭), lty=2, col = 'blue')
+abline( v = mean(merge_new$종가), lty=2, col = 'blue')
 abline( m1, lty=2, col='red')
 
 # 특정값 x의 y 예측
@@ -58,7 +62,7 @@ predict(m1, newdata = data.frame(종가 = 4))
 
 # 예측값 다뤄보기
 preidct <- fitted(m1)
-pre_merge <- cbind(merge,preidct)
+pre_merge <- cbind(merge_new,preidct)
 View(pre_merge)
 colnames(pre_merge)[4] <- '예측값'
 str(pre_merge)
@@ -68,4 +72,4 @@ max(pre_merge$차이)
 # 엑셀 파일로 저장
 install.packages("writexl")
 library(writexl)
-write_xlsx(pre_merge,path = "/Users/A/Desktop/일봉예측값_익일나스닥.xlsx")
+write_xlsx(pre_merge,path = "/Users/A/Desktop/일봉예측값_익일나스닥2020.xlsx")
